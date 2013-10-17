13af24724f9d2863baaffd8a602214ac2f4d7fcf
Michal Cichra <michal@o2h.cz>
2013-10-17 13:30:26 +0200

    cleanup formula
    * utilize superenv
    * warn about debugging headers
    * fix drizzle name
    * fix dav module option
    * add gunzip module option

--- Formula/ngx_openresty.rb
+++ Formula/ngx_openresty.rb
@@ -7,14 +7,20 @@ class NgxOpenresty < Formula
 
   depends_on 'pcre'
   depends_on 'luajit' => :recommended
-  depends_on 'libdrizzle' if build.include? 'with-drizzle'
-  depends_on 'postgresql' if build.include? 'with-postgres'
+  depends_on 'drizzle' => :optional
+  depends_on 'postgresql' => :optional
 
+  # openresty options
   option 'without-luajit', "Compile *without* support for the Lua Just-In-Time Compiler"
   option 'with-drizzle', "Compile with support for upstream communication with MySQL and/or Drizzle database servers"
-  option 'with-postgres', "Compile with support for direct communication with PostgreSQL database servers"
+  option 'with-postgresql', "Compile with support for direct communication with PostgreSQL database servers"
   option 'with-iconv', "Compile with support for converting character encodings"
-  option 'without-debug', "Compile *without* support for debug logging"
+
+  option 'with-debug', "Compile with support for debug logging and proper gdb debugging symbols"
+
+  # nginx options
+  option 'with-webdav', "Compile with ngx_http_dav_module"
+  option 'with-gunzip', "Compile with ngx_http_gunzip_module"
 
   skip_clean 'logs'
 
@@ -23,24 +29,30 @@ class NgxOpenresty < Formula
       "--with-http_ssl_module",
       "--with-pcre",
       "--with-pcre-jit",
-      "--with-cc-opt='-I#{HOMEBREW_PREFIX}/include'",
-      "--with-ld-opt='-L#{HOMEBREW_PREFIX}/lib'",
       "--sbin-path=#{bin}/openresty",
       "--conf-path=#{etc}/openresty/nginx.conf",
       "--pid-path=#{var}/run/openresty.pid",
       "--lock-path=#{var}/openresty/nginx.lock"
     ]
 
-    args << "--with-http_dav_module" if build.include? 'with-webdav'
-    args << "--with-debug" unless build.include? 'without-debug'
+    args << "--with-http_dav_module" if build.with? 'webdav'
+    args << "--with-http_gunzip_module" if build.with? 'gunzip'
+
+    # Debugging mode (with proper debugging symbols)
+    if build.with? 'debug'
+      args << '--with-debug'
+      opoo "Openresty will be built --with-debug option, but without debugging symbols. For debugging symbols you have to compile it by hand."
+    end
 
     # OpenResty options
-    args << "--with-luajit" unless build.include? 'without-luajit'
-    args << "--with-http_drizzle_module" if build.include? 'with-drizzle'
-    args << "--with-http_postgres_module" if build.include? 'with-postgres'
-    args << "--with-http_iconv_module" if build.include? 'with-iconv'
+    args << "--with-luajit" unless build.without? 'luajit'
+
+    args << "--with-http_drizzle_module" if build.with? 'drizzle'
+    args << "--with-http_postgres_module" if build.with? 'postgres'
+    args << "--with-http_iconv_module" if build.with? 'iconv'
 
     system "./configure", *args
+
     system "make"
     system "make install"
   end
